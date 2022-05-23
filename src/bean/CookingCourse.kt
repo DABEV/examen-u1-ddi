17810aa36d699f49064e35e39d5d6bc57328a394
gustavo lopez <thered04@outlook.com>
2022-05-23 17:54:39 -0500

    prueba

--- src/bean/CookingCourse.kt
+++ src/bean/CookingCourse.kt
@@ -1,4 +1,9 @@
 package bean
 
-class CookingCourse {
+class CookingCourse(id: Int, name: String,description: String) : Course(id, name, description) {
+    companion object{
+        private const val ERROR = ""
+        private const val SUCCESS = ""
+        private const val INFO = ""
+    }
 }
