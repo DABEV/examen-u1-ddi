feb7f26e4797aa89a6f9e59c0b34a8c2d3682fd1
Gandy Avila <20193tn128@utez.edu.mx>
2022-05-23 18:07:06 -0500

    Dao de Cooking Course

---
+++ src/dao/CookingCourseDao.kt
@@ -0,0 +1,41 @@
+package dao
+
+import bean.CookingCourse
+
+class CookingCourseDao (val list : MutableList<CookingCourse> = mutableListOf()) {
+    fun add (c : CookingCourse) : Boolean {
+        if (get(c.id) != null) {
+            list.add(c)
+            return true
+        }
+        return false
+    }
+
+    fun get (id: Int) : CookingCourse? {
+        for (c in list) {
+            if (c.id.equals(id))
+                return c
+        }
+        return null
+    }
+
+    fun update (c : CookingCourse) : Boolean {
+        for (i in 0 .. list.size-1) {
+            if (list[i].id.equals(c.id)) {
+                list[i] = c
+                return true
+            }
+        }
+        return false
+    }
+
+    fun delete (id : Int) : Boolean {
+        for (i in 0 .. list.size-1) {
+            if (list[i].id.equals(id)) {
+                list.removeAt(i)
+                return true
+            }
+        }
+        return false
+    }
+}
