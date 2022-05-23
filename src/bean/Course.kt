4e5f1d3ca2fe96262d3b6ff281d346653ce48e32
SpawnSkull <saposuper_14@hotmail.com>
2022-05-23 17:38:59 -0500

    Clase padre creada

---
+++ src/bean/Course.kt
@@ -0,0 +1,4 @@
+package bean
+
+open class Course(val id: Int, val name: String, val description: String) {
+}
---
+++ src/bean/ProgramingCourse.kt
@@ -0,0 +1,5 @@
+package bean
+
+class ProgramingCourse(id: Int, name: String,description: String) : Course(id, name, description) {
+
+}
